124da1edea34c0b6b622004815bf610a17344d27
Minachante99 <nelly550ruiz@gmail.com>
2026-01-09 22:20:39 -0600

    New changes.Facebook login updated.

--- facebook_login.py
+++ facebook_login.py
@@ -1,117 +1,90 @@
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import Chrome
+"""Script para automatizar el login en facebook mediante email y password.Se le puede pasar cookies.
+Mas adelante se le agregara la parte de loguearse en gmail para verificar correo."""
+
+from seleniumbase import Driver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-import joblib,time,random
+from datetime import datetime as dt
+import json,time,random,os
 
-def start_chrome(headless=False):
-    #opciones del navegador
-    options = Options()
-    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0')
-    options.add_argument('--headless') if headless else options.add_argument('--start-maximized')
-    options.add_argument('--disable-web-security')
-    options.add_argument('--disable-extensions')
-    options.add_argument('--disable-notifications')
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--log-level=3')
-    options.add_argument('--allow-running-insecure-content')
-    options.add_argument('--no-default-browser-check')
-    options.add_argument('--no-first-run')
-    options.add_argument('--no-proxy-server')
-    options.add_argument('--disable-blink-features=AutomationContolled')
-    options.add_experimental_option('excludeSwitches',['enable-automation','ignore-certificate-errors','enable-logging'])
-    options.add_experimental_option('prefs',{'profile.default_content_settings_values.notifications':2,'intl.accept_languages':['es-ES','es'],'credentials_enable_service':False})
-    driver = Chrome(options=options)
-
-    return driver
-
-def quit_driver(driver):
-    driver.close()
-    driver.quit()
+def errors_screenshoter(driver):
+	"""Para guardar un screenshot del punto donde dio un error."""
+	#si no existe la carpeta de errores la crea
+	if not os.path.isdir('Errors'):
+		os.mkdir('Errors')
+	curr_time = dt.now()
+	stringuinized = curr_time.strftime('%Y-%m-%d_%H-%M-%S') #formato year-month-days-hour-minute-secs
+	driver.save_screenshot('Errors' + os.sep + 'Error_' + stringuinized + '.png')
 
 def keys_sender(element,key):
-    #para simular humanidad a la hora de enviar texto
+    """Para simular humanidad a la hora de enviar texto"""
     for letter in key:
-        time.sleep(random.choice(range(1,5))/10)
+        time.sleep(random.randint(1,5)/10)
         element.send_keys(letter)
 
-def manual_login(driver,wait):
+def manual_login(email,password,driver,wait):
+    """Para loguearse manual en el face con correo y password."""
     driver.get('https://www.facebook.com')
     
     #correo
     try:
         correo_element = wait.until(ec.visibility_of_element_located((By.XPATH,'//input[@id="email"]')))
     except TimeoutException:
-        quit_driver(driver)
-        exit()
-    keys_sender(correo_element,'nelly550ruiz@gmail.com')
-    print('Correo: OK.')
-
+        errors_screenshoter(driver)
+        print('Algo fue mal al cargar el correo')
+        return 'bad'
+    keys_sender(correo_element,email)
     #password
     pass_element = driver.find_element(By.XPATH,'//input[@id="pass"]')
-    keys_sender(pass_element,'Leon.990423')
-    print('Password: OK.')
+    keys_sender(pass_element,password)
 
     #Boton de siguiente
     try:
-        next_button = wait.until(ec.element_to_be_clickable((By.XPATH,"//button[@name='login']")))
+        wait.until(ec.element_to_be_clickable((By.XPATH,"//button[@name='login']"))).click()
     except TimeoutException:
-        quit_driver(driver)
-        exit()
-    next_button.click()
-    print('Enviando claves...')
-    #por si tengo que poner el 2FA o algo:
-    #time.sleep(30)
-
-    #entrando a perfil
-    breakpoint()
-    try:    
-        wait.until(ec.visibility_of_element_located((By.XPATH,'//a[@aria-label="Biografía de Paulo Leon"]')))
-    except TimeoutException:
-        quit_driver(driver)
-        exit()
-
-    print('Perfil: OK.\n\nLogiado manual.')
-    #salvando cookies
-    with open('face_cookies.joblib','wb') as file:
-        joblib.dump(driver.get_cookies(),file)
+        errors_screenshoter(driver)
+        print('Algo fue mal al clickear el boton se siguiente.')
+        return 'bad'
     
-    time.sleep(5)
+    #entrando a perfil
+    try:    
+        wait.until(ec.visibility_of_element_located((By.XPATH,'//div[@aria-label="Tu perfil"]')))
+    except TimeoutException:
+        errors_screenshoter(driver)
+        print('Algo fue mal al cargar el perfil.')
+        return 'bad'
+    #salvando cookies
+    with open('face_cookies.json','w') as file:
+        json.dump(driver.get_cookies(),file)
+    
     return driver
 
-def login_face(headless=False,manual=False):
+def login_face(email,password):
+    """Funcion inicial que intenta loguearse con cookies, si falla llama al manual."""
     #starting chrome
-    driver = start_chrome()
+    driver = Driver(uc=True)
     wait = WebDriverWait(driver,10)
-    #para tests
-    if manual: 
-        return manual_login(driver,wait)
-        
-    driver.get('https://facebook.com/robots.txt')
-    #checkando si hay cookies
+    driver.maximize_window()
+    #buscando cookies primero
     try:
-        with open('face_cookies.joblib','rb') as file:
-            cookies = joblib.load(file)
+        driver.get('https://facebook.com/robots.txt')
+        time.sleep(1)
+        with open('face_cookies.json','r') as file:
+            cookies = json.load(file)
         for cookie in cookies: driver.add_cookie(cookie)
         driver.get('https://facebook.com')
-        wait.until(ec.visibility_of_element_located((By.XPATH,'//a[@aria-label="Biografía de Paulo Leon"]')))
-        print('Loguiado por cookies.')
-        time.sleep(3)
+        wait.until(ec.visibility_of_element_located((By.XPATH,'//div[@aria-label="Tu perfil"]')))
     except OSError:
-        driver = manual_login(driver,wait)
+        driver = manual_login(email,password,driver,wait)
     except TimeoutException:
-        driver = manual_login(driver,wait)
+        driver = manual_login(email, password,driver,wait)
     
     return driver
 
 if __name__ == '__main__':
-    driver = login_face()
-    input('Hablate ')
-    quit_driver(driver)
-    exit()
+    driver = login_face('nelly550ruiz@gmail.com','Leon.990423')
 
     
 
